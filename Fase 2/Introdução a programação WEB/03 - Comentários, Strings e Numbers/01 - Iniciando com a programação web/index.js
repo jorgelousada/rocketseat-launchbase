8d7894fc3560ee38268544dff25e51073ef53cc2
Jorge Lousada <jorgelousada2@gmail.com>
2020-07-29 14:51:40 -0300

    Comentários, Strings e Numbers
     Comentários, Strings e Numbers

---
+++ Fase 2/Introdução a programação WEB/03 - Comentários, Strings e Numbers/01 - Iniciando com a programação web/index.js
@@ -0,0 +1,12 @@
+/* 
+    Criar um programa que calcula a média das notas entre os alunos e 
+    envia mensagem do calculo da média.
+*/
+const aluno01 = 'Jorge'
+const aluno02 = 'Mayk'
+const notaAluno01 = 9.8
+const notaAluno02 = 10.0
+
+// Função que retorna o tipo da variavel em javascript
+console.log(typeof(aluno01))
+console.log(typeof(notaAluno01))
