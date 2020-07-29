6c3cb00d618e16fe71aa15436b29d5157b8a60f5
Jorge Lousada <jorgelousada2@gmail.com>
2020-07-29 14:52:53 -0300

    Estrutura de repetição
    Estrutura de repetição

---
+++ Fase 2/Introdução a programação WEB/11 - Estrutura de repetição/index.js
@@ -0,0 +1,65 @@
+/* 
+    Criar um programa que cria a média das turmas de alunos 
+    e envia mensagem do calculo da média
+*/
+
+const alunosDaTurmaA = [
+    {
+        nome: "Mayk",
+        nota: 9.8
+    },
+    {
+        nome: "Diego",
+        nota: 10
+    },
+    {
+        nome: "Fulano",
+        nota: 2.3
+    }
+];
+
+const alunosDaTurmaB = [
+    {
+        nome: "André",
+        nota: 10
+    },
+    {
+        nome: "Pedro",
+        nota: 7
+    },
+    {
+        nome: "Adriano",
+        nota: 9.5
+    }
+];
+
+
+function calculaMedia(alunos) {
+    var media = 0;
+    for (let aluno of alunos) {
+        media = media + aluno.nota;
+    }
+    return (media / alunos.length);
+}
+
+function calculaMedia2(alunos) {
+    var media = 0;
+    for (let idx = 0; idx<alunos.length; idx++) {
+        media = media + alunos[idx].nota;
+    }
+    return (media / alunos.length);
+}
+
+const mediaA = calculaMedia(alunosDaTurmaA);
+const mediaB = calculaMedia2(alunosDaTurmaB);
+
+function enviaMensagem(media,turma) {
+    if (media > 5) {
+        console.log(`A media da turma ${turma} foi de ${media}. Parabéns!`);
+    } else {
+        console.log('A media é menor que 5.');
+    }
+}
+
+enviaMensagem(mediaA,"A");
+enviaMensagem(mediaB,"B");
