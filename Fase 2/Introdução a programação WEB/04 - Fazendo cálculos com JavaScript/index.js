3eb4659288d0a8893d1eb5196de6a5759f8b6b22
Jorge Lousada <jorgelousada2@gmail.com>
2020-07-29 14:51:55 -0300

    Fazendo cálculos com JavaScript
    Fazendo cálculos com JavaScript

---
+++ Fase 2/Introdução a programação WEB/04 - Fazendo cálculos com JavaScript/index.js
@@ -0,0 +1,19 @@
+/* 
+    Criar um programa que calcula a média das notas entre os alunos e 
+    envia mensagem do calculo da média.
+
+    */
+   const aluno01 = 'Jorge'
+   const notaAluno01 = 9.8
+   
+   const aluno02 = 'Mayk'
+   const notaAluno02 = 10.0
+   
+   const aluno03 = 'Francisco'
+   const notaAluno03 = 5.5
+   
+   const media = (notaAluno01 + notaAluno02 + notaAluno03) / 3
+
+// Média da nota da turma
+console.log('A média da turma é: ' + media)
+
